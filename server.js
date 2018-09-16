a660eb034d4e8e7b9f3222e88f05fa4e48918b38
AnilVishvkarma2015 <AnilVishvkarma2015@users.noreply.github.com>
2018-09-16 14:01:56 +0530

    [RS-2001] Purchase order module implemented (#4)

--- server.js
+++ server.js
@@ -10,17 +10,18 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
 
-app.use(function(req, res, next) {
-   res.header("Access-Control-Allow-Origin", "*");
-   res.header('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST');
-   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-   next();
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
 });
 
 app.use(jwt());
 app.use('/users', require('./lib/users/user.controller'));
 app.use('/products', require('./lib/products/product.controller'));
 app.use('/suppliers', require('./lib/suppliers/supplier.controller'));
+app.use('/purchaseorders', require('./lib/purchaseorders/purchaseorder.controller'));
 app.use(errorHandler);
 
 const port = process.env.NODE_ENV === 'production' ? 80 : 4000;
