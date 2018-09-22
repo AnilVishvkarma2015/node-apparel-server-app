8dd1e54065c2648e9e326003b8f373d22b138a89
AnilVishvkarma2015 <AnilVishvkarma2015@users.noreply.github.com>
2018-09-22 21:54:02 +0530

    [RS-2003] Added config module for better configuration (#6)

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
-require('rootpath');
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const config = require('config');
+
 const jwt = require('./lib/shared/jwt');
 const errorHandler = require('./lib/shared/error-handler');
 const app = express();
@@ -9,14 +10,6 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
-
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST');
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
-
 app.use(jwt());
 app.use('/users', require('./lib/users/user.controller'));
 app.use('/products', require('./lib/products/product.controller'));
@@ -24,7 +17,7 @@ app.use('/suppliers', require('./lib/suppliers/supplier.controller'));
 app.use('/purchaseorders', require('./lib/purchaseorders/purchaseorder.controller'));
 app.use(errorHandler);
 
-const port = process.env.PORT || 4000;
-const server = app.listen(port, () => {
-    console.log("Server started and running on port:" + port);
+const port = config.get('port');
+app.listen(port, () => {
+    console.log("Application Server is running on port:" + port);
 })
